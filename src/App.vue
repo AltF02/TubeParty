90aa75d7e86e362e450a5fd20fb27a9ca47152d0
Matthew <Staalkoper@gmail.com>
2020-07-21 22:19:56 +0200

    oui

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+
   </div>
 </template>
 
-<style>
+<script>
+  import io from 'socket.io-client';
+
+  export default {
+    name: "Chat",
+    data() {
+      return {
+        socket: io('localhost:3001')
+      }
+    },
+    methods: {
+      sendMessage(e) {
+        e.preventDefault();
+
+        this.socket.emit('SEND_MESSAGE', {
+          user: this.user,
+          message: this.message
+        });
+        this.message = '';
+      }
+    },
+    mounted() {
+      this.socket.on('MESSAGE', (data) => {
+        this.messages = [...this.messages, data];
+      })
+    }
+  }
+</script>
+
+<style scoped>
 
 </style>
